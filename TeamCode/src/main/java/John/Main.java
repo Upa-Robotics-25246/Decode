985bb35cf5e4e0be20256204f79c25e899ac8b18
strange <strangejmaster@gmail.com>
2025-09-20 17:02:10 -0500

    Made simple motor mover

--- TeamCode/src/main/java/John/Main.java
+++ TeamCode/src/main/java/John/Main.java
@@ -1,4 +1,22 @@
 package John;
 
-public class Main {
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp
+public class Main extends OpMode {
+    DcMotor robert;
+
+    public void init() {
+        robert = hardwareMap.get(DcMotor.class, "motorNames");
+        robert.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void loop() {
+        if ( gamepad1.a == true ) {
+            robert.setPower(gamepad1.left_stick_y);
+        }
+    }
 }
