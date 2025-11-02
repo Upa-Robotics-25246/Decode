d067e6a5c63a214d056c7279265ad0f02dc1922f
invis8 <zwilliam462@gmail.com>
2025-11-02 13:53:33 -0500

    Added LimelightSubsystem that can display the detector results and turn towards the first artifact

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -1,45 +1,65 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.util.GoBildaPinpointDriver;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-
 
 public class Hardware {
+
     //singleton
     private static Hardware instance;
 
-    public final DcMotorEx intake;
-    public final Servo sorter;
+
+    //    // Motors
+
+    private Limelight3A limelight;
     public final DcMotorEx lf;
     public final DcMotorEx rf;
     public final DcMotorEx lb;
     public final DcMotorEx rb;
-    // Odometry
+
+//     Servos
+
+
+    public final CRServo turn;
+    public final CRServo turret;
+    public final DcMotorEx intake;
+    public final Servo pusher;
+
+    public final DcMotorEx shooter;
+
+    //     Odometry
     public final GoBildaPinpointDriver pinPointOdo;
 
-    public final Limelight3A limelight;
+    public Hardware(HardwareMap hwMap) {
 
-    public final WebcamName logitech;
 
-    public Hardware(HardwareMap hwMap){
         this.rf = hwMap.get(DcMotorEx.class, Specifications.FTRT_MOTOR); //rightforward
         this.lf = hwMap.get(DcMotorEx.class, Specifications.FTLF_MOTOR); //leftforward
         this.lb = hwMap.get(DcMotorEx.class, Specifications.BKLF_MOTOR); //leftback
-        this.rb = hwMap.get(DcMotorEx.class, Specifications.BKRT_MOTOR); //rightback
+        this.rb = hwMap.get(DcMotorEx.class, Specifications.BKRT_MOTOR);//rightback
+
+        this.pusher = hwMap.get(Servo.class, Specifications.PUSHER);
+        this.limelight = hwMap.get(Limelight3A.class, Specifications.LIME_LIGHT);
+
+
         this.intake = hwMap.get(DcMotorEx.class, Specifications.INTAKE);
-        this.sorter = hwMap.get(Servo.class, Specifications.SORTER);
+        this.turn = hwMap.get(CRServo.class, Specifications.TURN);
+        this.turret = hwMap.get(CRServo.class, Specifications.TURRET);
+        this.shooter = hwMap.get(DcMotorEx.class, Specifications.SHOOTER);
 
 
+
+//
         this.pinPointOdo = hwMap.get(GoBildaPinpointDriver.class, Specifications.PIN_POINT_ODOMETRY);
 
-        this.logitech = hwMap.get(WebcamName.class, "Webcam 1");
-
-        this.limelight = hwMap.get(Limelight3A.class, "limelight");
     }
 
     public static Hardware getInstance(HardwareMap hwMap) {
@@ -48,4 +68,8 @@ public class Hardware {
         }
         return instance;
     }
+
+
 }
+
+
