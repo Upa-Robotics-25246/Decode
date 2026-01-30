9b9fc1a54600dfef3b2b2d9a33801ff8fbfe9be9
Pranav Nigam <pn89348@users.noreply.github.com>
2026-01-30 01:17:24 -0800

    wahoo-sammy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/test/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/test/TurretTest.java
@@ -1,57 +1,62 @@
 package org.firstinspires.ftc.teamcode.util.test;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Turret first test")
+@Configurable
 public class TurretTest extends OpMode {
    DcMotorEx turret;
+   DcMotorEx intake;
    Servo hood;
    double hoodPos = 0;
-   double TurretPower = 0;
+   static double TurretPower = 0;
     @Override
     public void init() {
         turret = hardwareMap.get(DcMotorEx.class,"turret");
         turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         hood = hardwareMap.get(Servo.class,"hood");
+        intake = hardwareMap.get(DcMotorEx.class,"flywheel");
         hood.setPosition(0);
-        turret.setTargetPosition(0);
+
 
     }
 
     @Override
     public void loop() {
-        if (gamepad1.dpadUpWasPressed()){
-            hoodPos+=0.05;
-        }else if(gamepad1.dpadDownWasPressed()){
-            hoodPos-=0.05;
-        }
-        if (hoodPos>1){
-            hoodPos = 1;
-        }else if(hoodPos<0){
-            hoodPos = 0;
-        }
-        if(gamepad1.dpadRightWasPressed()){
-            hood.setPosition(hoodPos);
-
-        }
-        if (gamepad1.yWasPressed()){
-            TurretPower+=0.05;
-        }else if(gamepad1.aWasPressed()){
-            TurretPower-=0.05;
-        }
-        if (TurretPower>1) {
-            TurretPower = 1;
-        } else if(TurretPower<-1){
-            TurretPower = -1;
-        }
-        if(gamepad1.xWasPressed()){
+//        if (gamepad1.dpadUpWasPressed()){
+//            hoodPos+=0.05;
+//        }else if(gamepad1.dpadDownWasPressed()){
+//            hoodPos-=0.05;
+//        }
+//        if (hoodPos>1){
+//            hoodPos = 1;
+//        }else if(hoodPos<0){
+//            hoodPos = 0;
+//        }
+//        if(gamepad1.dpadRightWasPressed()){
+//            hood.setPosition(hoodPos);
+//
+//        }
+//        if (gamepad1.yWasPressed()){
+//            TurretPower+=0.05;
+//        }else if(gamepad1.aWasPressed()){
+//            TurretPower-=0.05;
+//        }
+//        if (TurretPower>1) {
+//            TurretPower = 1;
+//        } else if(TurretPower<-1){
+//            TurretPower = -1;
+//        }
+//        if(gamepad1.xWasPressed()){
 
             turret.setPower(TurretPower);
+            intake.setPower(TurretPower);
 
-        }
+       // }
 
         telemetry.addData("hoodPos: ",hoodPos);
         telemetry.addData("TurretPower: ",TurretPower);
