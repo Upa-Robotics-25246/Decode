53906c2d2799ac59f3317b0e60046df73a96a4fe
awesumdude42 <sammyiyers@gmail.com>
2026-01-29 19:27:50 -0800

    added panels, hopefully flywheel pid tuning goes well

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/test/mercurial1Stuff/FlywheelPIDTune.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/test/mercurial1Stuff/FlywheelPIDTune.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.util.test.mercurial1Stuff;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -11,7 +12,7 @@ import com.seattlesolvers.solverslib.controller.wpilibcontroller.SimpleMotorFeed
 
 import org.firstinspires.ftc.teamcode.util.pedro.Constants;
 import org.firstinspires.ftc.teamcode.util.pedro.Poses;
-
+@Configurable
 public class FlywheelPIDTune extends OpMode {
     DcMotorEx flywheel;
 
@@ -27,45 +28,38 @@ public class FlywheelPIDTune extends OpMode {
     static double kD = 0;
     static double kS = 0;
     static double kV = 0;
-    public static double velocity =0;
+    static double velocity =0;
 
 
-    //NEED ODO FOR DISTANCE, ONLY WAY THIS WILL WORK
+
     @Override
     public void init() {
         flywheel = hardwareMap.get(DcMotorEx.class, "transfer");
         flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(startPose);
 
         feedforward = new SimpleMotorFeedforward(kS, kV);
         Feedback = new PIDController(kP, kI, kD);
     }
 
+
     @Override
     public void loop() {
 
         Feedback.setPID(kP, kI, kD);
-        //velocity is gotten from the regression, change once you get the regression working
+
         double power = Feedback.calculate(flywheel.getVelocity(), velocity);
         double ff = feedforward.calculate(velocity);
 
-        //hoodPos is also gotten from the regression
 
 
-        follower.update();
-        double absX = trackPoint.getX() - follower.getPose().getX();
-        double absY = trackPoint.getY() - follower.getPose().getY();
 
-        double dist = Math.sqrt(
-                ((Math.pow(absX, 2)) + (Math.pow(absY, 2)))
-        );
+
+
 
 
         flywheel.setPower(power + ff);
         telemetry.addData("Power", flywheel.getPower());
         telemetry.addData("Velocity", flywheel.getVelocity());
-        telemetry.addData("distance", dist);
     }
 
 }
